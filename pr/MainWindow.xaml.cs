e62233035afca4b8387c30a09105c78322fa1bf8
ann <panikarova065@gmail.com>
2024-01-15 23:41:49 +0300

    начало

--- pr/MainWindow.xaml.cs
+++ pr/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,23 @@ using System.Windows.Shapes;
 
 namespace pr
 {
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+    }
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=panUsers;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+
+    }
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -23,6 +41,56 @@ namespace pr
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            reg reg = new reg();
+            reg.Show();
+            this.Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var login = logTB.Text;
+            var password = passTB.Password;
+
+            var context = new AppDbContext();
+
+            var user = context.Users.SingleOrDefault(x => (x.Login == login || x.Email == login)  && x.Password == password);
+            logTB.BorderBrush = new SolidColorBrush(Colors.Black);
+            passTB.BorderBrush = new SolidColorBrush(Colors.Black);
+            if (user is null)
+            {
+                //passTB.Password = pass.Text;
+                //pass.Visibility = Visibility.Visible;
+                //passTB.Visibility = Visibility.Hidden;
+                var user_exists = context.Users.FirstOrDefault(x => x.Login == login);
+                if (user_exists is null)
+                {
+                    logTB.BorderBrush = new SolidColorBrush(Colors.Red);
+                    error.Text = "Такого пользователя не существует";
+
+                }
+                else
+                {
+                    passTB.BorderBrush = new SolidColorBrush(Colors.Red);
+                    error.Text = "Неверный пароль";
+                }
+                return;
+            }
+            
+            MessageBox.Show("Вы успешно вошли в аккаунт!");
+            kabinet kabinet = new kabinet();
+            kabinet.Show();
+            this.Close();
+            kabinet.name.Text = $"Здравствуйте, { user.Login} ";
         }
     }
 }
---
+++ pr/reg.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace pr
+{
+    /// <summary>
+    /// Логика взаимодействия для reg.xaml
+    /// </summary>
+    public partial class reg : Window
+    {
+        public reg()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var login = logTB.Text;
+            var pass = passTB.Password;
+            var emal = emailTB.Text;
+            var context = new AppDbContext();
+            var user_exists = context.Users.FirstOrDefault(x => x.Login == login);
+            if (user_exists is not null)
+            {
+                MessageBox.Show("Такой пользователь уже вошел");
+                return;
+            }
+            var user = new User { Email = emal, Login = login, Password = pass };
+            context.Users.Add(user);
+            context.SaveChanges();
+            MessageBox.Show("Вы успешно зарегистрировались");
+
+            MainWindow taskWindow = new MainWindow();
+            taskWindow.Show();
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
